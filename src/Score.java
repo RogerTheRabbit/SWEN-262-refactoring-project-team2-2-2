198f24971f1e311fadeaf24a6af86314904c5f4c
djb7559 <djb7559@rit.edu>
2020-04-10 15:27:21 -0400

    finished formatting

--- src/Score.java
+++ src/Score.java
@@ -1,5 +1,4 @@
 /**
- *
  * To change this generated comment edit the template variable "typecomment":
  * Window>Preferences>Java>Templates. To enable and disable the creation of type
  * comments go to Window>Preferences>Java>Code Generation.
@@ -7,29 +6,29 @@
 
 public class Score {
 
-	private String nick;
-	private String date;
-	private String score;
+    private String nick;
+    private String date;
+    private String score;
 
-	public Score(String nick, String date, String score) {
-		this.nick = nick;
-		this.date = date;
-		this.score = score;
-	}
+    public Score(String nick, String date, String score) {
+        this.nick = nick;
+        this.date = date;
+        this.score = score;
+    }
 
-	public String getNickName() {
-		return nick;
-	}
+    public String getNickName() {
+        return nick;
+    }
 
-	public String getDate() {
-		return date;
-	}
+    public String getDate() {
+        return date;
+    }
 
-	public String getScore() {
-		return score;
-	}
+    public String getScore() {
+        return score;
+    }
 
-	public String toString() {
-		return nick + "\t" + date + "\t" + score;
-	}
+    public String toString() {
+        return nick + "\t" + date + "\t" + score;
+    }
 }
