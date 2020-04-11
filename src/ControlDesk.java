a13219191f39a5df9fc349fab1303899e420f312
mjh9131 <mjh9131@rit.edu>
2020-04-10 21:03:55 -0400

    Alley deleted

--- src/ControlDesk.java
+++ src/ControlDesk.java
@@ -63,7 +63,6 @@ class ControlDesk extends Thread {
      * @param numlanes	the numbler of lanes to be represented
      *
      */
-
 	public ControlDesk(int numLanes) {
 		this.numLanes = numLanes;
 		lanes = new HashSet(numLanes);
