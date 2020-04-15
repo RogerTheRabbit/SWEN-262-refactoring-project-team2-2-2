0782a56f3237cbc3ba3d3c0706a336c1f2a86eef
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2020-04-15 13:50:32 -0400

    Fixed variables in Running

--- src/LaneState/LaneStatus.java
+++ src/LaneState/LaneStatus.java
@@ -4,10 +4,11 @@ import temp.LaneEvent;
 import temp.LaneObserver;
 import temp.Party;
 import temp.Pinsetter;
+import temp.PinsetterEvent;
 
 public interface LaneStatus {
     public void run();
-    public void receivePinsetterEvent();
+    public void receivePinsetterEvent(PinsetterEvent pinsetterEvent);
     public void assignParty(Party theParty);
     public boolean isPartyAssigned();
     public void subscribe(LaneObserver adding);
