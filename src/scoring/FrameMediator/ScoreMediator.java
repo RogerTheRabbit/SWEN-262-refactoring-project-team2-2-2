a2e82537dd88877ea2a4a98f052aceafeed304b4
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2020-04-17 15:33:00 -0400

    Commented ScoreMediator

--- src/scoring/FrameMediator/ScoreMediator.java
+++ src/scoring/FrameMediator/ScoreMediator.java
@@ -5,6 +5,9 @@ import temp.Bowler;
 
 import java.util.HashMap;
 
+/**
+ * This class is responsible for managing scoring.
+ */
 public class ScoreMediator {
     private HashMap<Bowler, Frame[]> frames;
 
@@ -12,6 +15,12 @@ public class ScoreMediator {
         this.frames = new HashMap<>();
     }
 
+    /**
+     * Adds a player to the lane to start keeping track
+     * of their bowling scores.
+     * 
+     * @param bowler the bowler to be added
+     */
     public void addPlayer(Bowler bowler){
         Frame[] newScores = new Frame[10];
         for(int i = 0; i < 9; i++){
@@ -21,17 +30,33 @@ public class ScoreMediator {
         this.frames.put(bowler, newScores);
     }
 
+    /**
+     * Returns if a player can throw again for a given frame.
+     * 
+     * @param bowler Bowler to check
+     * @param frame Frame to check
+     * @return true if bowler can throw again false otherwise
+     */
     public boolean canThrowAgain(Bowler bowler, int frame){
         Frame currentFrame = this.frames.get(bowler)[frame];
         return !currentFrame.getFinished();
     }
 
+    /**
+     * Resets the scores for all the bowlers in the lane.
+     */
     public void resetGame(){
         for(Bowler bowler: frames.keySet()){
             this.addPlayer(bowler);
         }
     }
 
+    /**
+     * Handles a throw made by a bowler
+     * 
+     * @param bowler Bowler that made throw
+     * @param ballThrown Ball that they threw
+     */
     public void addThrow(Bowler bowler, int ballThrown){
         //get the frames for a specific bowler
         Frame[] throwerFrames = this.frames.get(bowler);
@@ -48,6 +73,12 @@ public class ScoreMediator {
         }
     }
 
+    /**
+     * Returns all of the throws that the bowler made.
+     * 
+     * @param bowler Bowler to get throws for
+     * @return Throws made by the bowler
+     */
     public int[] getAllThrows(Bowler bowler){
         Frame[] bowlerFrames = frames.get(bowler);
         int[] allThrows = new int[21];
@@ -62,6 +93,12 @@ public class ScoreMediator {
         return allThrows;
     }
 
+    /**
+     * Gets an array of all the points for each frame.
+     * 
+     * @param bowler Bowler to get frame points for
+     * @return An array where each value is the score for that frame.
+     */
     public int[] getFramePoints(Bowler bowler){
         Frame[] bowlerFrames = frames.get(bowler);
         int[] framePoints = new int[10];
